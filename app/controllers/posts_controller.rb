4133f5946f837ff908ca48c1fb492328cd9392de
Kazuyuki Matsumura <kazuyukimatsumura@hiroshima-u.ac.jp>
2019-02-26 19:12:19 +0900

    パスにhelper_methodを追加しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,17 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
-  def shows
+  def show
     @post = Post.find_by(id: params[:id])
   end
   def new
+    @post = Post.new
   end
   def create
     @post = Post.new(content: params[:content])
-
     @post.save
-    redirect_to("/posts/index")
+    redirect_to posts_index_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get "top" => "home#top"
 
   get "posts/index" => "posts#index"
